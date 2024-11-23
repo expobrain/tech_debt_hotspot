eb8f2b3586c6e40c8f3d6e28f2c77cc40cf2c57c
Daniele Esposti <daniele@withplum.com>
2024-11-23 00:46:16 +0000

    Added sort CLI option

--- src/main.rs
+++ src/main.rs
@@ -7,16 +7,68 @@ use std::path::PathBuf;
 
 use clap::{arg, command, value_parser};
 use stats::{collect_changes_count_from_path, collect_stats_from_path, get_git_base_path};
+use types::SortBy;
 
 use crate::types::FileStats;
+
+fn sort_stats_by(stats: &mut [FileStats], sort_by: SortBy) {
+    match sort_by {
+        SortBy::Path => {
+            stats.sort_unstable_by(|a, b| a.path.cmp(&b.path));
+        }
+        SortBy::MaintainabilityIndex => {
+            stats.sort_unstable_by(|a, b| {
+                b.maitainability_index
+                    .partial_cmp(&a.maitainability_index)
+                    .unwrap_or(std::cmp::Ordering::Equal)
+            });
+        }
+        SortBy::HalsteadVolume => {
+            stats.sort_unstable_by(|a, b| {
+                b.halstead_volume
+                    .partial_cmp(&a.halstead_volume)
+                    .unwrap_or(std::cmp::Ordering::Equal)
+            });
+        }
+        SortBy::CyclomaticComplexity => {
+            stats.sort_unstable_by(|a, b| {
+                b.cyclomatic_complexity
+                    .partial_cmp(&a.cyclomatic_complexity)
+                    .unwrap_or(std::cmp::Ordering::Equal)
+            });
+        }
+        SortBy::LinesOfCode => {
+            stats.sort_unstable_by(|a, b| {
+                b.loc
+                    .partial_cmp(&a.loc)
+                    .unwrap_or(std::cmp::Ordering::Equal)
+            });
+        }
+        SortBy::CommentsPercentage => {
+            stats.sort_unstable_by(|a, b| {
+                b.comments_percentage
+                    .partial_cmp(&a.comments_percentage)
+                    .unwrap_or(std::cmp::Ordering::Equal)
+            });
+        }
+        SortBy::ChangesCount => {
+            stats.sort_unstable_by(|a, b| {
+                b.changes_count
+                    .partial_cmp(&a.changes_count)
+                    .unwrap_or(std::cmp::Ordering::Equal)
+            });
+        }
+    }
+}
 
 fn main() {
     let matches = command!("tech_debt_hotspot")
         .arg(
-            arg!([DIRECTORY])
+            arg!(<DIRECTORY>)
                 .required(true)
                 .value_parser(value_parser!(PathBuf)),
         )
+        .arg(arg!(-s --sort <SORT>).value_parser(value_parser!(SortBy)))
         .get_matches();
 
     let directory = matches
@@ -24,13 +76,14 @@ fn main() {
         .unwrap()
         .canonicalize()
         .expect("Failed to canonicalize path");
+    let sort_by = *matches
+        .get_one::<SortBy>("sort")
+        .unwrap_or(&SortBy::MaintainabilityIndex);
 
     if !directory.is_dir() || directory.read_dir().is_err() {
         eprintln!("Error: {} is not a directory", directory.display());
         std::process::exit(1);
     }
-
-    println!("Using input file: {}", directory.display());
 
     let mut stats = HashMap::new();
 
@@ -43,13 +96,11 @@ fn main() {
         }
     }
 
-    let git_base_path = get_git_base_path(&directory);
     let mut stats_vec: Vec<FileStats> = stats.values().cloned().collect();
-    stats_vec.sort_unstable_by(|a, b| {
-        b.maitainability_index
-            .partial_cmp(&a.maitainability_index)
-            .unwrap_or(std::cmp::Ordering::Equal)
-    });
+
+    sort_stats_by(&mut stats_vec, sort_by);
+
+    let git_base_path = get_git_base_path(&directory);
 
     for stat in &mut stats_vec {
         stat.path = PathBuf::from(&stat.path)
--- src/types.rs
+++ src/types.rs
@@ -1,5 +1,6 @@
 use std::fmt;
 
+use clap::{builder::PossibleValue, ValueEnum};
 use tabled::Tabled;
 
 #[derive(Debug, Tabled, Clone, Default)]
@@ -28,4 +29,54 @@ pub struct FileStats {
     pub comments_percentage: f64,
     pub maitainability_index: f64,
     pub changes_count: u32,
+}
+
+#[derive(Clone, Copy, Debug)]
+pub enum SortBy {
+    Path,
+    MaintainabilityIndex,
+    HalsteadVolume,
+    CyclomaticComplexity,
+    LinesOfCode,
+    CommentsPercentage,
+    ChangesCount,
+}
+
+// Can also be derived with feature flag `derive`
+impl ValueEnum for SortBy {
+    fn value_variants<'a>() -> &'a [Self] {
+        &[
+            SortBy::Path,
+            SortBy::MaintainabilityIndex,
+            SortBy::HalsteadVolume,
+            SortBy::CyclomaticComplexity,
+            SortBy::LinesOfCode,
+            SortBy::CommentsPercentage,
+            SortBy::ChangesCount,
+        ]
+    }
+
+    fn to_possible_value(&self) -> Option<PossibleValue> {
+        Some(match self {
+            SortBy::Path => PossibleValue::new("path").help("Sort by path"),
+            SortBy::MaintainabilityIndex => {
+                PossibleValue::new("maintainability_index").help("Sort by maintainability index")
+            }
+            SortBy::HalsteadVolume => {
+                PossibleValue::new("halstead_volume").help("Sort by Halstead volume")
+            }
+            SortBy::CyclomaticComplexity => {
+                PossibleValue::new("cyclomatic_complexity").help("Sort by cyclomatic complexity")
+            }
+            SortBy::LinesOfCode => {
+                PossibleValue::new("lines_of_code").help("Sort by lines of code")
+            }
+            SortBy::CommentsPercentage => {
+                PossibleValue::new("comments_percentage").help("Sort by comments percentage")
+            }
+            SortBy::ChangesCount => {
+                PossibleValue::new("changes_count").help("Sort by changes count")
+            }
+        })
+    }
 }
